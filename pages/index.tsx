e240e7461f7c08ece2244e7bfb1215d7476a54c8
Tim-fx <notfelix.exe@gmail.com>
2022-11-26 16:41:01 +0700

    Index, Login, Premium

--- pages/index.tsx
+++ pages/index.tsx
@@ -12,24 +12,21 @@ export default function Home() {
       <main className='bg-slate-900 h-screen'>
         <nav className="bg-slate-900 shadow">
           <div className="container flex items-center justify-center p-6 capitalize text-gray-300">
-            <Link href="./pages/index.html" className="transition-colors duration-300 transform text-gray-200 border-b-2 border-green-500 mx-1.5 sm:mx-6">home</Link>
-            <Link href="./premium.html" className="border-b-2 border-transparent transition-colors duration-300 transform hover:text-gray-200 hover:border-green-500 mx-1.5 sm:mx-6">Premium</Link>
-            <Link href="./login.html" className="relative border-b-2 border-transparent transition-colors duration-300 transform hover:text-gray-200 hover:border-green-500 mx-1.5 sm:mx-6">Login</Link>
+            <Link href="/" className="transition-colors duration-300 transform text-gray-200 border-b-2 border-green-500 mx-1.5 sm:mx-6">Home</Link>
+            <Link href="/premium" className="border-b-2 border-transparent transition-colors duration-300 transform hover:text-gray-200 hover:border-green-500 mx-1.5 sm:mx-6">Premium</Link>
+            <Link href="/login" className="relative border-b-2 border-transparent transition-colors duration-300 transform hover:text-gray-200 hover:border-green-500 mx-1.5 sm:mx-6">Login</Link>
             <Link href="./register.html" className="border-b-2 border-transparent transition-colors duration-300 transform hover:text-gray-200 hover:border-green-500 mx-1.5 sm:mx-6">Register</Link>
           </div>
         </nav>
-        <section className="bg-gray-900 text-white">
-          <div className="mx-auto max-w-screen-xl px-4 py-16 sm:px-6 lg:px-8">
-            <div className="mx-auto max-w-lg text-center">
-              <h2 className="text-3xl font-semibold sm:text-3xl">Choose Your <span className="text-green-500">Subject</span></h2>
-
-              <p className="mt-4 text-gray-300">
-                <br/><br/>
-              </p>
+        <section className="bg-slate-900 text-white">
+          <div className="mx-auto max-w-screen-xl px-4 py-20 sm:px-6 lg:px-8">
+            <div className="mx-auto max-w-screen-xl text-center">
+              <h2 className="text-3xl mb-6 font-semibold sm:text-3xl">Daftar <span className="text-green-500">Pelajaran</span></h2>
+              <p className='text-lg font-medium mb-12'>Tonton video, baca materi, dan kerjakan quiz untuk meningkatkan level mu</p>
             </div>
 
             <div className="mt-8 grid grid-cols-1 gap-8 md:grid-cols-2 lg:grid-cols-3">
-              <a className="block rounded-xl border border-gray-800 p-8 shadow-xl transition hover:border-pink-500/10 hover:shadow-pink-500/10"
+              <a className="bg-slate-800 block rounded-xl border hover:bg-green-500 border-gray-800 p-8 shadow-xl transition hover:border-pink-500/10 hover:shadow-pink-500/10"
                 href="/matematika.html">
                 <svg xmlns="http://www.w3.org/2000/svg" className="h-10 w-10 text-green-500" fill="none" viewBox="0 0 24 24"
                   stroke="currentColor">
@@ -39,15 +36,10 @@ export default function Home() {
                   <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
                     d="M12 14l9-5-9-5-9 5 9 5zm0 0l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14zm-4 6v-7.5l4-2.222" />
                 </svg>
-
-                <h2 className="mt-4 text-xl font-bold text-white">Matematika</h2>
-
-                <p className="mt-1 text-sm text-gray-300">
-                  <br/><br/>
-                </p>
+                <h2 className="mt-4 mb-10 text-xl font-bold text-white">Matematika</h2>
               </a>
 
-              <a className="block rounded-xl border border-gray-800 p-8 shadow-xl transition hover:border-pink-500/10 hover:shadow-pink-500/10"
+              <a className="bg-slate-800 block rounded-xl border hover:bg-green-500 border-gray-800 p-8 shadow-xl transition hover:border-pink-500/10 hover:shadow-pink-500/10"
                 href="/fisika.html">
                 <svg xmlns="http://www.w3.org/2000/svg" className="h-10 w-10 text-green-500" fill="none" viewBox="0 0 24 24"
                   stroke="currentColor">
@@ -57,14 +49,10 @@ export default function Home() {
                   <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
                     d="M12 14l9-5-9-5-9 5 9 5zm0 0l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14zm-4 6v-7.5l4-2.222" />
                 </svg>
-
                 <h2 className="mt-4 text-xl font-bold text-white">Fisika</h2>
-
-                <p className="mt-1 text-sm text-gray-300">
-                </p>
               </a>
 
-              <a className="block rounded-xl border border-gray-800 p-8 shadow-xl transition hover:border-pink-500/10 hover:shadow-pink-500/10"
+              <a className="bg-slate-800 block rounded-xl border hover:bg-green-500 border-gray-800 p-8 shadow-xl transition hover:border-pink-500/10 hover:shadow-pink-500/10"
                 href="/kimia.html">
                 <svg xmlns="http://www.w3.org/2000/svg" className="h-10 w-10 text-green-500" fill="none" viewBox="0 0 24 24"
                   stroke="currentColor">
@@ -74,14 +62,10 @@ export default function Home() {
                   <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
                     d="M12 14l9-5-9-5-9 5 9 5zm0 0l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14zm-4 6v-7.5l4-2.222" />
                 </svg>
-
                 <h2 className="mt-4 text-xl font-bold text-white">Kimia</h2>
-
-                <p className="mt-1 text-sm text-gray-300">
-                </p>
               </a>
 
-              <a className="block rounded-xl border border-gray-800 p-8 shadow-xl transition hover:border-pink-500/10 hover:shadow-pink-500/10"
+              <a className="bg-slate-800 block rounded-xl border hover:bg-green-500 border-gray-800 p-8 shadow-xl transition hover:border-pink-500/10 hover:shadow-pink-500/10"
                 href="/biologi.html">
                 <svg xmlns="http://www.w3.org/2000/svg" className="h-10 w-10 text-green-500" fill="none" viewBox="0 0 24 24"
                   stroke="currentColor">
@@ -91,15 +75,10 @@ export default function Home() {
                   <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
                     d="M12 14l9-5-9-5-9 5 9 5zm0 0l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14zm-4 6v-7.5l4-2.222" />
                 </svg>
-
-                <h2 className="mt-4 text-xl font-bold text-white">Biologi</h2>
-
-                <p className="mt-1 text-sm text-gray-300">
-                  <br/><br/>
-                </p>
+                <h2 className="mt-4 mb-10 text-xl font-bold text-white">Biologi</h2>
               </a>
 
-              <a className="block rounded-xl border border-gray-800 p-8 shadow-xl transition hover:border-pink-500/10 hover:shadow-pink-500/10"
+              <a className="bg-slate-800 block rounded-xl border hover:bg-green-500 border-gray-800 p-8 shadow-xl transition hover:border-pink-500/10 hover:shadow-pink-500/10"
                 href="/utbk.html">
                 <svg xmlns="http://www.w3.org/2000/svg" className="h-10 w-10 text-green-500" fill="none" viewBox="0 0 24 24"
                   stroke="currentColor">
@@ -109,14 +88,10 @@ export default function Home() {
                   <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
                     d="M12 14l9-5-9-5-9 5 9 5zm0 0l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14zm-4 6v-7.5l4-2.222" />
                 </svg>
-
                 <h2 className="mt-4 text-xl font-bold text-white">UTBK</h2>
-
-                <p className="mt-1 text-sm text-gray-300">
-                </p>
               </a>
 
-              <a className="block rounded-xl border border-gray-800 p-8 shadow-xl transition hover:border-pink-500/10 hover:shadow-pink-500/10"
+              <a className="bg-slate-800 block rounded-xl border hover:bg-green-500 border-gray-800 p-8 shadow-xl transition hover:border-pink-500/10 hover:shadow-pink-500/10"
                 href="/mandiri.html">
                 <svg xmlns="http://www.w3.org/2000/svg" className="h-10 w-10 text-green-500" fill="none" viewBox="0 0 24 24"
                   stroke="currentColor">
@@ -126,16 +101,8 @@ export default function Home() {
                   <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
                     d="M12 14l9-5-9-5-9 5 9 5zm0 0l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14zm-4 6v-7.5l4-2.222" />
                 </svg>
-
                 <h2 className="mt-4 text-xl font-bold text-white">Mandiri</h2>
-
-                <p className="mt-1 text-sm text-gray-300">
-                </p>
               </a>
-            </div>
-
-            <div className="mt-12 text-center">
-
             </div>
           </div>
         </section>
